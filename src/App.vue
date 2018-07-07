a9bd9e36ba9208cdf57a2bf6953d9e66f2b034ce
nickwu241 <nickwu241@gmail.com>
2018-07-07 01:18:14 -0400

    Center site at max-width 960, add blog section

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,99 @@
 <template>
   <div>
     <!-- NAVBAR -->
-    <b-navbar toggleable="md" type="dark" variant="dark" sticky class="px-md-5">
-      <b-navbar-toggle target="nav_collapse" />
+    <div class="bg-dark">
+      <b-navbar toggleable="md" type="dark" variant="dark" sticky class="header-container">
+        <b-navbar-toggle target="nav_collapse" />
 
-      <b-navbar-brand href="#" @click="navClicked({title: 'Projects'})">
-        <img class="rounded-circle" src="https://avatars3.githubusercontent.com/u/9021054?v=4&u=de203ee9993bc2e6533c96ef7f689888ade10d3f&s=45" /> Nicholas Wu
-      </b-navbar-brand>
+        <b-navbar-brand href="#" @click="navClicked({title: 'Projects'})">
+          <img class="rounded-circle" src="https://avatars3.githubusercontent.com/u/9021054?v=4&u=de203ee9993bc2e6533c96ef7f689888ade10d3f&s=45" /> Nicholas Wu
+        </b-navbar-brand>
 
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav>
-          <b-nav-item v-for="item in navItems" :key="item.title" @click="navClicked(item)" :class="{ active : item.isActive }">
-            {{item.title}}
-          </b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto" style="flex-direction: row;">
-          <b-nav-item>
-            <a class="fa fa-linkedin" href="https://linkedin.com/in/nick-wu/"></a>
-          </b-nav-item>
-          <b-nav-item>
-            <a class="fa fa-github" href="https://github.com/nickwu241/"></a>
-          </b-nav-item>
-          <b-nav-item>
-            <a class="fa fa-instagram" href="https://www.instagram.com/nickwu241/"></a>
-          </b-nav-item>
-          <b-nav-item>
-            <a class="fa fa-youtube" href="https://www.youtube.com/user/nickwu241"></a>
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item v-for="item in navItems" :key="item.title" @click="navClicked(item)" :class="{ active : item.isActive }">
+              {{item.title}}
+            </b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav style="flex-direction: row;">
+            <b-nav-item href="https://linkedin.com/in/nick-wu/">
+              <span class="fa fa-linkedin" />
+            </b-nav-item>
+            <b-nav-item href="https://linkedin.com/in/nick-wu/">
+              <span class="fa fa-github" />
+            </b-nav-item>
+            <b-nav-item href="https://medium.com/@nickwu241">
+              <span class="fa fa-medium" />
+            </b-nav-item>
+            <b-nav-item href="https://www.instagram.com/nickwu241">
+              <span class="fa fa-instagram" />
+            </b-nav-item>
+            <b-nav-item href="https://www.youtube.com/user/nickwu241">
+              <span class="fa fa-youtube" />
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
 
     <!-- CONTENT -->
-    <b-container fluid id="content-container" class="light-background px-md-5">
-      <b-row>
-        <b-col class="mt-md-4" />
-      </b-row>
-
-      <!-- Projects -->
-      <b-row v-if="activeItemTitle === 'Projects'" align-content="center">
-        <b-col v-for="p in projects" :key="p.title" sm="12" md="6" lg="4" class="px-0 px-md-2 mb-2 mb-md-4">
-          <b-card :title="p.name" :sub-title="p.subtitle" :img-src="p.imgsrc[0]" :img-alt="p.name" img-top>
-            <p class="card-text">{{ p.description }}</p>
-            <b-button v-if="p.demolink" :href="p.demolink" variant="success">Try It</b-button>
-            <b-button v-if="p.videosrc" :href="p.videosrc[0]" variant="danger">Watch on YouTube</b-button>
-            <b-button :href="p.ghlink" variant="primary">View on GitHub</b-button>
-          </b-card>
-        </b-col>
-      </b-row>
-      <!-- Resume -->
-      <b-row v-else-if="activeItemTitle === 'Resume'" align-h="center">
-        <b-col xl="6" class="pt-2 section-content">
-          <p> Click
-            <a href="../resume.pdf">here</a>
-            to view my resume if the PDF plugin is not supported by your device.
-          </p>
-          <embed src="../resume.pdf#toolbar=0&navpanes=0&scrollbar=0&view=fit" type="application/pdf" width="100%" :height="resumeHeight">
-        </b-col>
-      </b-row>
-      <!-- About -->
-      <b-row v-else-if="activeItemTitle === 'About'" align-h="center">
-        <b-col xl="6" class="pt-2 section-content">
-          <p>Hi sneaky visitors, I'm flattered that you want to learn more about me! :)</p>
-          <P>If you can't find me hacking away at projects, then I'll most likely be playing soccer, guitar, board games, video games, or watching Netflix.</P>
-          <p>Happy Birthday! Here's some development tips and tricks for you!</p>
-          <Gist />
-        </b-col>
-      </b-row>
-
-      <b-row>
-        <b-col class="mt-md-2" />
-      </b-row>
+    <b-container fluid class="bg-lightblue p-md-3">
+      <div class="content-container">
+        <!-- Projects -->
+        <b-row v-if="activeItemTitle === 'Projects'" align-content="center">
+          <b-col v-for="p in projects" :key="p.title" sm="12" md="6" class="px-0 px-md-2 mb-2 mb-md-4">
+            <b-card :title="p.name" :sub-title="p.subtitle" :img-src="p.imgsrc[0]" :img-alt="p.name" img-top>
+              <p class="card-text">{{ p.description }}</p>
+              <b-button v-if="p.demolink" :href="p.demolink" variant="success">Try It</b-button>
+              <b-button v-if="p.videosrc" :href="p.videosrc[0]" variant="danger">Watch on YouTube</b-button>
+              <b-button :href="p.ghlink" variant="primary">View on GitHub</b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+        <!-- Blog -->
+        <b-row v-if="activeItemTitle === 'Blog'">
+          <b-col class="p-0 section-content">
+            <b-list-group v-for="post in posts" :key="post.title">
+              <b-list-group-item :href="post.link" class="flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                  <h5 class="mb-1">{{post.title}}</h5>
+                  <small class="text-muted">{{post.date}}</small>
+                </div>
+                <p class="mb-1">
+                  {{post.summary}}
+                </p>
+                <small class="text_muted">{{post.source}} • {{post.duration}} read</small>
+              </b-list-group-item>
+            </b-list-group>
+          </b-col>
+        </b-row>
+        <!-- Resume -->
+        <b-row v-else-if="activeItemTitle === 'Resume'">
+          <b-col class="pt-2 section-content">
+            <p> Click
+              <a href="../resume.pdf">here</a>
+              to view my resume if the PDF plugin is not supported by your device.
+            </p>
+            <embed src="../resume.pdf#toolbar=0&navpanes=0&scrollbar=0&view=fit" type="application/pdf" width="100%" :height="resumeHeight">
+          </b-col>
+        </b-row>
+        <!-- About -->
+        <b-row v-else-if="activeItemTitle === 'About'">
+          <b-col class="pt-2 section-content">
+            <p>Hi sneaky visitors, I'm flattered that you want to learn more about me! :)</p>
+            <P>If you can't find me hacking away at projects, then I'll most likely be playing soccer, guitar, board games, video games, or watching Netflix.</P>
+            <p>Happy Birthday! Here's some development tips and tricks for you!</p>
+            <Gist />
+          </b-col>
+        </b-row>
+      </div>
     </b-container>
-
   </div>
 </template>
 
 <script>
 import Gist from './Gist.vue'
+import posts from './posts.json'
 import projects from './projects.json'
 
 export default {
@@ -91,9 +107,11 @@ export default {
       resumeHeight: 0,
       navItems: [
         { title: 'Projects', isActive: true },
+        { title: 'Blog', isActive: false },
         { title: 'Resume', isActive: false },
         { title: 'About', isActive: false }
       ],
+      posts,
       projects
     }
   },
@@ -118,7 +136,13 @@ export default {
   color: #3e4e57;
 }
 
-.light-background {
+.header-container,
+.content-container {
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.bg-lightblue {
   background: #ebf5fa;
 }
 
@@ -135,18 +159,22 @@ export default {
   object-fit: contain;
 }
 
-@media (min-width: 768px) {
-  .card-img-top {
-    height: 20vw;
-  }
-}
-
 .fa {
-  margin: 0.25rem;
+  margin-right: 0.6rem;
   padding: 0.6rem;
   text-align: center;
   text-decoration: none;
   border-radius: 50%;
+}
+
+@media (min-width: 768px) {
+  .card-img-top {
+    height: 20vw;
+  }
+
+  .fa {
+    margin: 0;
+  }
 }
 
 .fa:hover {
@@ -162,6 +190,11 @@ export default {
 
 .fa-github {
   background-color: black;
+  color: white;
+}
+
+.fa-medium {
+  background-color: green;
   color: white;
 }
 
