5fd87a40358f092f99d190ea3de6bbaa1220d2a3
Nick Wu <nickwu241@gmail.com>
2019-06-03 00:47:28 -0700

    Finish adding Nuxt.js

---
+++ nickwu/layouts/default.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <nav-header/>
+    <b-container fluid class="p-md-3 content-container">
+      <nuxt></nuxt>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import NavHeader from '~/components/NavHeader.vue'
+
+export default {
+  components: {
+    NavHeader
+  }
+}
+</script>
+
+<style>
+/* Social Media buttons for Contact section */
+@import '~/assets/bootstrap-social.css';
+
+/* Fill up the whole height with a background colour */
+html,
+body {
+  min-height: 100%;
+  background: #d4effc;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #3e4e57;
+}
+
+
+.header-container,
+.content-container {
+  max-width: 1080px;
+  margin: 0 auto;
+}
+
+.fa-round {
+  margin-right: 0.6rem;
+  padding: 0.6rem;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 50%;
+  color: white;
+}
+
+@media (min-width: 768px) {
+  .fa-round {
+    margin: 0;
+  }
+}
+
+.fa-round:hover {
+  opacity: 0.7;
+  color: white;
+  text-decoration: none;
+}
+
+.fa-linkedin-bg {
+  background-color: #007bb5;
+}
+
+.fa-github-bg {
+  background-color: black;
+}
+
+.fa-medium-bg {
+  background-color: green;
+}
+
+.fa-instagram-bg {
+  background-color: #bc2a8d;
+}
+
+.fa-youtube-bg {
+  background-color: #cc181e;
+}
+</style>
---
+++ nickwu/nuxt.config.js
@@ -0,0 +1,71 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Nicholas Wu',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'author', content: pkg.author },
+      { name: 'keywords', content: 'Nicholas Wu, Nicholas, Nick, Wu, nickwu241' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' }
+      // <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700|Abril+Fatface">
+    ]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+    // Doc: https://github.com/nuxt-community/analytics-module
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-125077009-1'
+    }]
+  ],
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    // extend(config, ctx) {
+    //   // Run ESLint on save
+    //   if (ctx.isDev && ctx.isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
+  }
+}
