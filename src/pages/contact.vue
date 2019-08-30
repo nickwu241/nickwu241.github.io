20e001aff490ea6f305e7148b968cd20fcbb9437
Nick Wu <nickwu241@gmail.com>
2019-08-29 18:03:07 -0700

    Update portfolio: add introduction, embed videos

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -2,10 +2,7 @@
   <div class="flex justify-center">
     <div class="w-screen md:w-2/3">
       <div class="m-2">
-        <p class="text-4xl">
-          👋 Hi, I'm Nick!
-        </p>
-        <p class="md:text-lg">
+        <!-- <p class="md:text-lg">
           I'm a Software Engineer with 2+ years of industry experinece and a
           degree in Computer Engineering (minor in Commerce) from UBC.
           <br />
@@ -17,8 +14,8 @@
           <br />
           On this site, you can to check out some of my work and articles or
           learn more about me. Enjoy! :)
-        </p>
-        <p class="text-lg">Experience</p>
+        </p> -->
+        <p class="text-lg mt-4">Experience</p>
         <LogoBullet
           imgsrc="images/icons/shopify.png"
           title="Shopify (8 months)"
@@ -39,53 +36,63 @@
           title="Safe Software (8 months)"
           subtitle="Software Developer Intern - Platforms"
         />
-        <p class="text-lg">Education</p>
+        <p class="text-lg mt-4">Education</p>
         <LogoBullet
           imgsrc="images/icons/ubc.png"
           title="University of British Columbia"
           subtitle="Bachelor of Applied Science, Computer Engineering"
         />
-
-        <p>
-          Looking for a Developer? Check out my work and get in touch!
-        </p>
-        <p class="text-xl">
-          Social:
-          <a class="inline-block" href="https://linkedin.com/in/nick-wu">
+        <p class="text-lg mt-4 mb-2">Contact</p>
+        <a href="mailto:nickwu241@gmail.com" target="_blank">
+          <button class="btn bg-red-500 hover:bg-red-700">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
+              width="24"
+              height="24"
               viewBox="0 0 24 24"
-              class="rounded text-blue-700 fill-current"
+              class="fill-current mr-2"
+            >
+              <path
+                d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"
+              />
+            </svg>
+            nickwu241@gmail.com
+          </button>
+        </a>
+        <a href="https://linkedin.com/in/nick-wu" target="_blank">
+          <button class="btn bg-blue-500 hover:bg-blue-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              class="fill-current mr-2"
             >
               <title>LinkedIn</title>
               <path
-                d="M0 0v24h24v-24h-24zm8 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.397-2.586 7-2.777 7 2.476v6.759z"
+                d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
               />
             </svg>
-          </a>
-          <a class="inline-block" href="https://www.instagram.com/nickwu241">
+            nick-wu
+          </button>
+        </a>
+        <a href="https://www.instagram.com/nickwu241" target="_blank">
+          <button class="btn bg-purple-500 hover:bg-purple-700">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
+              width="24"
+              height="24"
               viewBox="0 0 24 24"
-              class="rounded text-purple-700 fill-current"
+              class="fill-current mr-2"
             >
               <title>Instagram</title>
               <path
-                d="M14.667 12c0 1.473-1.194 2.667-2.667 2.667-1.473 0-2.667-1.193-2.667-2.667 0-1.473 1.194-2.667 2.667-2.667 1.473 0 2.667 1.194 2.667 2.667zm3.846-3.232c.038.843.046 1.096.046 3.232s-.008 2.389-.046 3.233c-.1 2.15-1.109 3.181-3.279 3.279-.844.038-1.097.047-3.234.047-2.136 0-2.39-.008-3.232-.046-2.174-.099-3.181-1.132-3.279-3.279-.039-.845-.048-1.098-.048-3.234s.009-2.389.047-3.232c.099-2.152 1.109-3.181 3.279-3.279.844-.039 1.097-.047 3.233-.047s2.39.008 3.233.046c2.168.099 3.18 1.128 3.28 3.28zm-2.405 3.232c0-2.269-1.84-4.108-4.108-4.108-2.269 0-4.108 1.839-4.108 4.108 0 2.269 1.84 4.108 4.108 4.108 2.269 0 4.108-1.839 4.108-4.108zm1.122-4.27c0-.53-.43-.96-.96-.96s-.96.43-.96.96.43.96.96.96c.531 0 .96-.43.96-.96zm6.77-7.73v24h-24v-24h24zm-4 12c0-2.172-.009-2.445-.048-3.298-.131-2.902-1.745-4.52-4.653-4.653-.854-.04-1.126-.049-3.299-.049s-2.444.009-3.298.048c-2.906.133-4.52 1.745-4.654 4.653-.039.854-.048 1.127-.048 3.299 0 2.173.009 2.445.048 3.298.134 2.906 1.746 4.521 4.654 4.654.854.039 1.125.048 3.298.048s2.445-.009 3.299-.048c2.902-.133 4.522-1.745 4.653-4.654.039-.853.048-1.125.048-3.298z"
+                d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
               />
             </svg>
-          </a>
-        </p>
-        <p class="text-xl">
-          Email:
-          <a class="text-blue-600" href="mailto:nickwu241@gmail.com"
-            >nickwu241@gmail.com
-          </a>
-        </p>
+            nickwu241
+          </button>
+        </a>
       </div>
     </div>
   </div>
@@ -102,7 +109,7 @@ export default {
 </script>
 
 <style scoped>
-.social-media-svg {
-  @apply rounded;
+.btn {
+  @apply inline-flex text-white font-bold p-3 rounded-lg mb-2 mr-1;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="flex flex-wrap justify-center">
+    <div class="w-full lg:w-9/12 m-4 p-2 md:px-10 md:pb-5">
+      <p class="text-4xl">
+        👋 Hi, I'm Nick!
+      </p>
+      <p class="text-lg sm:text-xl">
+        <span class="font-semibold"> Software Engineer </span> | Previously at
+        <br class="sm:hidden" />
+        <span class="break-keep-all">
+          <img
+            src="images/icons/shopify.png"
+            class="object-center object-scale-down inline w-6 h-6"
+          />
+          Shopify,
+        </span>
+        <span>
+          <img
+            src="images/icons/hootsuite.png"
+            class="object-center object-scale-down inline w-6 h-6"
+          />
+          Hootsuite,
+        </span>
+        <span>
+          <img
+            src="images/icons/safesoftware.png"
+            class="object-center object-scale-down inline w-6 h-6"
+          />
+          Safe Software
+        </span>
+      </p>
+      <p class="text-lg sm:text-xl mt-2">
+        <span class="font-semibold">UBC '19</span> | Computer Engineering,
+        <br class="sm:hidden" />
+        Dean's Honour List
+      </p>
+    </div>
     <div
       v-for="project in projects"
       :key="project.name"
